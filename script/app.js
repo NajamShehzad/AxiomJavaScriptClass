8ba874bf2f0da583922be4563c7dee58046781db
NajamShehzad <najambutt195@gmail.com>
2019-04-25 09:11:47 +0500

    JavaScript Class 24/4/2019

---
+++ script/app.js
@@ -0,0 +1,60 @@
+// var cities = ["Karachi", "Lahore", "Islamabad", "Quetta"];
+// var cityToCheck = prompt("Enter city name");
+
+
+// for(var i =0 ; i < cities.length ; i++){
+//     if(cityToCheck.toUpperCase() === cities[i].toUpperCase()){
+//         alert(cities[i])
+//     }
+// }
+
+
+// var month = prompt("Enter Month name");
+// var shortName;
+// if(month.length > 3){
+//     shortName = month.slice(0,3);
+// }
+
+// console.log(shortName);
+
+// var sen = "the New Yorker magazine doesn't allow the phrase World War II ";
+// var length = sen.length;
+// for (var i = 0; i < length; i++) {
+//     if (sen.slice(i, i + 12) == "World War II") {
+//         console.log("Before Change ==>>",sen);
+//         sen = sen.slice(0, i) + "The second world war " + sen.slice(i + 12);
+//         console.log("After slice ==>>",sen);
+//     }
+// }
+
+// var sen = "the New Yorker magazine doesn't allow the phrase World War II allow ";
+// var length = sen.length;
+// var index = sen.indexOf("World War II");
+// console.log("index ==>>",index)
+// var newSen = sen.slice(0, index) + " the second world war" + sen.slice(index + 12);
+// console.log("before ==>>",sen)
+// console.log("after ==>",newSen);
+
+
+// var sen = "My Name is faraz";
+// console.log(sen.charAt(5));
+
+
+// var sen = "the New Yorker World  War II magazine doesn't allow the phrase World War II ";
+// sen = sen.replace(/World War II/g,"The second world war");
+// console.log(sen);
+
+// var days = prompt("Enter Days")
+// var weeks = Math.floor(days/7)
+// console.log("Weeks ==>>",weeks);
+// console.log("Days ==>", days % 7);
+
+
+
+
+
+
+
+
+
+
